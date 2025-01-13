89ba94fbf342516fc7d45a265f72f660af47d725
Paula Ribeiro <contatopaular@gmail.com>
2025-01-13 04:45:46 -0300

    Alterado o endpoint do swagger

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('/', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
